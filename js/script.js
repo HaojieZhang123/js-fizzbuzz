3af37aa120599ea00d14bf8e6f2117786dbf22b3
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-18 14:08:22 +0100

    [UPD] correction of if controls. Misinterpreted the request. Excercise completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 // creation of for cycle for 100 iterations
 for (let i=0; i<100; i++) {
+    // controllo se multiplo di 3
     if((i+1)%3 == 0){
+        // controllo se multiplo di 5
+        if((i+1)%5 == 0){
+            console.log("Buzz");
+            continue;
+        }
         console.log("Fizz");
     }
-    else if((i+1)%5 == 0){
-        console.log("Buzz");
+    else {
+        console.log(i+1);
     }
-
 }
