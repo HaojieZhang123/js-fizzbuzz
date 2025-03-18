8f21410c85fff7178b2f0937efd65068be753d13
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-18 14:14:48 +0100

    [UPD] Did not understand the request correctly. Fixed again

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ for (let i=0; i<100; i++) {
     if((i+1)%3 == 0){
         // controllo se multiplo di 5
         if((i+1)%5 == 0){
-            console.log("Buzz");
+            console.log("FizzBuzz");
             continue;
         }
         console.log("Fizz");
+    }
+    else if((i+1)%5 == 0){
+    // controllo se multiplo di 5
+        console.log("Buzz");
+        continue;
     }
     else {
         console.log(i+1);
